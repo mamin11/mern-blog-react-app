23dae946e1f133507893687f5e4323cc7139b9bc
mamin11 <abdimamin@gmail.com>
2021-02-08 17:38:37 +0000

    create post working. can send request to backend

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,15 +2,21 @@ import React, {useState} from 'react';
 import useStyles from './styles';
 import FileBase from 'react-file-base64';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { createPost } from '../../actions/posts';
 
 const Form = () => {
     const [postData, setPostData] = useState({
         creator: '', title: '', subTitle: '', message: '', selectedFile: ''
     });
     const classes = useStyles();
+    const dispatch = useDispatch();
 
-    const handleSubmit = () => {
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        dispatch(createPost(postData));
     }
 
     const clear = () => {
